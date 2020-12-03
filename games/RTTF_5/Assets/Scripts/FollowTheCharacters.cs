4b796059e063f4168294560cd49783745bdce988
David Johnston <dvdjoh@gmail.com>
2020-12-02 21:43:05 -0500

    Add level and write camera

---
+++ games/RTTF_5/Assets/Scripts/FollowTheCharacters.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTheCharacters : MonoBehaviour
+{
+    private GameObject[] players;
+    private Camera cam;
+    public float minimumDistance = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = GameObject.FindGameObjectsWithTag("Player");
+        cam = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float maxPlayerDistance = 0f;
+        Vector3 averagePosition = Vector3.zero;
+        foreach(var player in players)
+        {
+            averagePosition += player.transform.position;
+
+            foreach (var other in players)
+            {
+                float distance = Vector3.Distance(player.transform.position, other.transform.position);
+                if(distance > maxPlayerDistance)
+                {
+                    maxPlayerDistance = distance;
+                }
+            }
+        }
+
+        averagePosition /= players.Length;
+
+        cam.transform.SetPositionAndRotation(averagePosition, cam.transform.rotation);
+        cam.orthographicSize = Mathf.Max(maxPlayerDistance, minimumDistance);
+    }
+}
