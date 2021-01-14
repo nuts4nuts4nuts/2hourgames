cefbd2a34d200d1ec7873bc9d8e12eae4fa3789c
David Johnston <dvdjoh@gmail.com>
2021-01-13 22:41:38 -0500

    Add some more koreography stuff

---
+++ games/RTTF_6/Assets/KoreographedColorChanger.cs
@@ -0,0 +1,31 @@
+﻿using SonicBloom.Koreo;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoreographedColorChanger : MonoBehaviour
+{
+    [EventID]
+    public string eventID;
+    public Color targetColor;
+
+    private Color startColor;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Koreographer.Instance.RegisterForEventsWithTime(eventID, SetColor);
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        startColor = spriteRenderer.color;
+    }
+
+    void SetColor(KoreographyEvent evt, int sampleTime, int sampleDelta, DeltaSlice deltaSlice)
+    {
+        if(evt.HasCurvePayload())
+        {
+            float curveValue = evt.GetValueOfCurveAtTime(sampleTime);
+            spriteRenderer.color = startColor * (1 - curveValue) + curveValue * targetColor; 
+        }
+    }
+}
---
+++ games/RTTF_6/Assets/Scripts/KoreographedBumper.cs
@@ -0,0 +1,30 @@
+﻿using SonicBloom.Koreo;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KoreographedScale : MonoBehaviour
+{
+    [EventID]
+    public string eventID;
+    public Transform moveTo;
+    public Vector3 targetScale;
+
+    private Vector3 startScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Koreographer.Instance.RegisterForEventsWithTime(eventID, SetScale);
+        startScale = transform.localScale;
+    }
+
+    void SetScale(KoreographyEvent evt, int sampleTime, int sampleDelta, DeltaSlice deltaSlice)
+    {
+        if(evt.HasCurvePayload())
+        {
+            float curveValue = evt.GetValueOfCurveAtTime(sampleTime);
+            transform.localScale = startScale * (1 - curveValue) + curveValue * targetScale; 
+        }
+    }
+}
